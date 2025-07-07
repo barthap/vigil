3ac54b205db4f647cca80bd31e2cfeb2dd8bf841
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-07-07 21:42:45 +0200

    Don't crash when substituted env is invalid

--- src/config/reader.rs
+++ src/config/reader.rs
@@ -22,14 +22,20 @@ impl ConfigReader {
         debug!("reading config file: {}", &APP_ARGS.config);
 
         // Read configuration
-        let mut conf = fs::read_to_string(&APP_ARGS.config).expect("cannot find config file");
+        let raw_conf = fs::read_to_string(&APP_ARGS.config).expect("cannot find config file");
 
         debug!("read config file: {}", &APP_ARGS.config);
 
         // Replace environment variables
         let environment = env::vars().collect::<HashMap<String, String>>();
 
-        conf = substitute(&conf, &environment).expect("cannot substitute environment variables");
+        let conf = match substitute(&raw_conf, &environment) {
+            Ok(substituted) => substituted,
+            Err(err) => {
+                warn!("Config env substitute failed: {err}");
+                raw_conf
+            }
+        };
 
         // Parse configuration
         let config = toml::from_str(&conf).expect("syntax error in config file");
@@ -51,7 +57,7 @@ impl ConfigReader {
 
         for service in config.probe.service.iter() {
             // Service identifier was already previously inserted? (caught a duplicate)
-            if service_identifiers.insert(&service.id) == false {
+            if !service_identifiers.insert(&service.id) {
                 panic!(
                     "configuration has duplicate service identifier: {}",
                     service.id
@@ -63,7 +69,7 @@ impl ConfigReader {
 
             for node in service.node.iter() {
                 // Node identifier was already previously inserted? (caught a duplicate)
-                if node_identifiers.insert(&node.id) == false {
+                if !node_identifiers.insert(&node.id) {
                     panic!(
                         "configuration has duplicate node identifier: {} in service: {}",
                         node.id, service.id
